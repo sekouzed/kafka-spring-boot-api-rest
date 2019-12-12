93e59f33f59ec0e72f70116c57d7ad5be6b7a06f
Sekou Dioubate <dioubatesekou@gmail.com>
2019-12-12 19:15:34 +0100

    backup

--- src/main/java/com/sekouzed/kafkaspringbootapirest/controller/MessageController.java
+++ src/main/java/com/sekouzed/kafkaspringbootapirest/controller/MessageController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "messages")
 @RestController
 @RequestMapping(value = "/messages")
 public class MessageController {
